e1ddc10c94a14b86a79d1d1e7874eb5d2d19cf14
hier09lyph <v.varona89@gmail.com>
2023-04-23 10:27:06 +0200

    refactor: rename selectors in app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 var taskInput = document.getElementById("new-task"); //Add a new task.
 var addButton = document.getElementsByTagName("button")[0]; //first button
-var incompleteTaskHolder = document.getElementById("incompleteTasks"); //ul of #incompleteTasks
+var incompleteTaskHolder = document.getElementById("incomplete-tasks"); //ul of #incompleteTasks
 var completedTasksHolder = document.getElementById("completed-tasks"); //completed-tasks
 
 
@@ -83,9 +83,9 @@ var editTask = function() {
   var listItem = this.parentNode;
 
   var editInput = listItem.querySelector('input[type=text]');
-  var label = listItem.querySelector("label");
-  var editBtn = listItem.querySelector(".edit");
-  var containsClass = listItem.classList.contains("editMode");
+  var label = listItem.querySelector(".task-item__label");
+  var editBtn = listItem.querySelector(".task-item__edit-btn");
+  var containsClass = listItem.classList.contains("task-item--edit-mode");
   //If class of the parent is .editmode
   if (containsClass) {
 
@@ -99,7 +99,7 @@ var editTask = function() {
   }
 
   //toggle .editmode on the parent.
-  listItem.classList.toggle("editMode");
+  listItem.classList.toggle("task-item--edit-mode");
 };
 
 
